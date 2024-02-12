f6ae3c5394c6d6a9ca5513ddb3272dfb05dbe0cc
SamuelAirs <sdayers@mail.fhsu.edu>
2024-02-11 19:41:18 -0600

    started building out first graph

--- main.py
+++ main.py
@@ -6,4 +6,30 @@ import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
 
+def getClosing(ticker):
+    stock = yf.Ticker(ticker)
+    hist = stock.history(period="10d")
+
+    closingList = []
+
+    for day in hist:
+        closingList.append((day['Date'], day['Close']))
+
+    print(closingList)
+
+    return closingList
+
+stocks = ["MSFT","AMZN","TSLA","AMD","AAPL"]
+
+msftClosing = np.array(getClosing("MSFT"))
+
+
+days = list(range(1, len(msftClosing)+1))
+
+plt.plot(days, msftClosing)
+plt.axis([1,10])
+plt.xlabel("Days")
+plt.ylabel("Closing Price")
+plt.title("Closing Price for " + "MSFT")
+plt.show()
 
